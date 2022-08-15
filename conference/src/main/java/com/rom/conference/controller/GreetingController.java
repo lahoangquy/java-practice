8ad0779c93ef9b25fec76bc076060fe9b8b54a95
lahoangquy <62043234+lahoangquy@users.noreply.github.com>
2022-08-15 21:36:46 +0930

    Add files via upload

---
+++ conference/src/main/java/com/rom/conference/controller/GreetingController.java
@@ -0,0 +1,15 @@
+package com.rom.conference.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Map;
+
+@Controller
+public class GreetingController {
+    @GetMapping("greeting")
+    public String greeting(Map<String,Object> model){
+    model.put("message","Hello Jack La");
+    return "greeting";
+    }
+}
